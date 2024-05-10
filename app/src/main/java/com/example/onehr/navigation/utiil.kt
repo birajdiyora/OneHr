fb89ae00677db08a420b1c3bba3b4bc378bde5e3
Biraj Diyora <birajdiyora1@gmail.com>
2024-05-10 13:12:47 +0530

    Package Updated

--- app/src/main/java/com/example/onehr/navigation/utiil.kt
+++ app/src/main/java/com/example/onehr/navigation/utiil.kt
@@ -3,7 +3,7 @@ package com.example.onehr.navigation
 import androidx.navigation.NavController
 
 fun navigateTo(navController: NavController, route : String){
-
+    //Hello
     navController.navigate(route){
 
         launchSingleTop=true
